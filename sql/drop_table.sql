cb190df4efa5459e957609f58c97561e8dfee54c
zuizhongmeng <youkoutou@gmail.com>
2024-11-23 13:59:33 +0900

    【标题】更新 【内容】 【其他】

---
+++ sql/drop_table.sql
@@ -0,0 +1,36 @@
+--drop table
+
+--quartz.sql
+DROP TABLE IF EXISTS QRTZ_FIRED_TRIGGERS;
+DROP TABLE IF EXISTS QRTZ_PAUSED_TRIGGER_GRPS;
+DROP TABLE IF EXISTS QRTZ_SCHEDULER_STATE;
+DROP TABLE IF EXISTS QRTZ_LOCKS;
+DROP TABLE IF EXISTS QRTZ_SIMPLE_TRIGGERS;
+DROP TABLE IF EXISTS QRTZ_SIMPROP_TRIGGERS;
+DROP TABLE IF EXISTS QRTZ_CRON_TRIGGERS;
+DROP TABLE IF EXISTS QRTZ_BLOB_TRIGGERS;
+DROP TABLE IF EXISTS QRTZ_TRIGGERS;
+DROP TABLE IF EXISTS QRTZ_JOB_DETAILS;
+DROP TABLE IF EXISTS QRTZ_CALENDARS;
+
+--ry_20230223.sql
+drop table if exists sys_dept;
+drop table if exists sys_user;
+drop table if exists sys_post;
+drop table if exists sys_role;
+drop table if exists sys_menu;
+drop table if exists sys_user_role;
+drop table if exists sys_role_menu;
+drop table if exists sys_role_dept;
+drop table if exists sys_user_post;
+drop table if exists sys_oper_log;
+drop table if exists sys_dict_type;
+drop table if exists sys_dict_data;
+drop table if exists sys_config;
+drop table if exists sys_logininfor;
+drop table if exists sys_job;
+drop table if exists sys_job_log;
+drop table if exists sys_notice;
+drop table if exists gen_table;
+drop table if exists gen_table_column;
+
